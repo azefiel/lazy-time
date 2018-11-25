fef1a932d29bdf525d5a23fe6c8f5e00824c86a1
Joris Tirado <joris@tirado.com>
2018-11-25 15:26:06 -0800

    Fix typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Formats time into verbose, lazy time.
  *
- * @params {Date} date Date to format into lazy format.
+ * @param {Date} date Date to format into lazy format.
  * @return {String} Representation of time in lazy format.
  */
 module.exports = function(date = new Date()) {
